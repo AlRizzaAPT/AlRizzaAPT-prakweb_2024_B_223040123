c57553b81bbf3e27f8d7d9ad543aaec4174db193
AlRizzaAchmad <acmadal@gmail.com>
2024-10-20 15:42:41 +0700

    video6

--- MVC/app/views/about/index.php
+++ MVC/app/views/about/index.php
@@ -1,4 +1,5 @@
 
     <h1>About Me </h1>
+    <img src="<?= BASEURL; ?>/img/kucing.jpg" class="rounded-circle" style="width: 200px;" alt="Riza">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>
 tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
